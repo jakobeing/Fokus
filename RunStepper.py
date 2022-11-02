88b75f75150e952379c3da7e3013184da8cefa13
jakobeing <77110568+jakobeing@users.noreply.github.com>
2022-11-02 21:16:48 +0100

    Version 3

--- RunStepper.py
+++ RunStepper.py
@@ -3,13 +3,15 @@ import time
 from DRV8825 import DRV8825
 import sys
 
+print("test 1")
+
 Wegstrecke_Positiv = int(0)
 Wegstrecke_Negativ = int(0)
 
 
 Motor2 = DRV8825(dir_pin=24, step_pin=18, enable_pin=4, mode_pins=(21, 22, 27))
 
-
+print("test 2")
 # Absolute Bewegung in Rellation zu 0
 if sys.argv[1] == "absolut":
     soll_possition = int(sys.argv[2])                            # Werte werden gehohlt.
@@ -19,12 +21,13 @@ if sys.argv[1] == "absolut":
     datei = open('List-Fokus .rtf','w')                          # Neuer wert wird gschrieben
     datei.write(str(soll_possition))                             # Neuer wert wird gschrieben
     datei.close()                                                # Neuer wert wird gschrieben
+    print("test 3")
     if soll_possition < Aktuelle_position:
         Wegstrecke_Negativ = Aktuelle_position - soll_possition
-
+        print("test 4")
     else:
         Wegstrecke_Positiv = soll_possition - Aktuelle_position
-
+        print("test 5")
 
 # Rellatieve bewegung in abhängigkeit vom aktuellen Standort (+, -)
 if sys.argv[1] == "Rellativ_+":
@@ -37,7 +40,7 @@ if sys.argv[1] == "Rellativ_+":
     datei.write(Neue_position)                           # Postionswert wird überschrieben
     datei.close()
     Wegstrecke_Positiv = Wegstrecke                      # ÜBERGABEWERT: Neue_position
-
+    print("test 6")
 
 if sys.argv[1] == "Rellativ_-":
     Wegstrecke = int(sys.argv[2])                        # Motoransteuerungswert (Positiv)
@@ -49,19 +52,18 @@ if sys.argv[1] == "Rellativ_-":
     datei.write(Neue_position)                           # Postionswert wird überschrieben
     datei.close()
     Wegstrecke_Negativ = Wegstrecke                      # ÜBERGABEWERT: Neue_position
-
-if  
+    print("test 7")
 
 
 
 Motor2.SetMicroStep('softward', 'halfstep')
 Motor2.TurnStep(Dir='forward', steps=Wegstrecke_Positiv, stepdelay=0.0010)
-print("Jes")
+print("test 8")
 time.sleep(0.5)
 Motor2.TurnStep(Dir='backward', steps=Wegstrecke_Negativ, stepdelay=0.0010)
 Motor2.Stop()
 
-
+print("test 9")
 """                                     
 # 1.8 degree: nema23, nema14            
 # softward Control :                    
@@ -73,9 +75,12 @@ Motor2.Stop()
 # '1/32step': A cycle = 200 * 32 steps  
 """
 
-exit()
+
 # except:
 #    # GPIO.cleanup()
 #    print("\nMotor stop")
 #    Motor2.Stop()
-#    exit()
+
+#     exit()
+
+exit()
